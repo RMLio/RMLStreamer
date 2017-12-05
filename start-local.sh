c4bf7a93160e4f316829dd0268b4f4235ed83ed1
wmaroy <maroy.wouter@gmail.com>
2017-12-05 18:14:29 +0100

    added bash scripts

--- start-local.sh
+++ start-local.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #../flink/build-target/bin/start-local.sh 
 bash ../Downloads/flink-1.3.2/bin/start-local.sh 
--- stop-local.sh
+++ stop-local.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/wmaroy/bin/env bash
 
 bash ../Downloads/flink-1.3.2/bin/stop-local.sh 
