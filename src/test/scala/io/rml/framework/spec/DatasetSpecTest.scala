a18d65c78d627dffbedaa58c2a579f0298189cb6
Wouter Maroy <wouter.maroy@ugent.be>
2018-02-05 16:57:13 +0100

    initialized test files

---
+++ src/test/scala/io/rml/framework/spec/DatasetSpecTest.scala
@@ -0,0 +1,15 @@
+package io.rml.framework.spec
+
+import org.scalatest.{FunSuite, Matchers}
+
+object DatasetSpecTest extends FunSuite with Matchers {
+
+  test("example1") {
+
+  }
+
+  test("example1") {
+
+  }
+
+}
