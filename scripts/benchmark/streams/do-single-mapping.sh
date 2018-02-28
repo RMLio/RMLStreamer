0085d87aa0e7382a54137789eace530f2e3fe1e3
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2018-02-28 13:49:46 +0100

    update for parsing delay results

--- scripts/benchmark/streams/do-single-mapping.sh
+++ scripts/benchmark/streams/do-single-mapping.sh
@@ -9,9 +9,6 @@ SUFFIX=$6
 AMOUNT_TRIPLES=$7
 MEASUREMENTS_FILE=$8
 
-echo "id,time" > $DATA_FOLDER/in.csv
-echo "id,time" > $DATA_FOLDER/out.csv
-
 docker run --name stream-in -d --net=host -v $DATA_FOLDER:/data stream-scripts streamFromFile.js /data/$INPUT_FILE_STREAM 5005 $DELAY $SUFFIX /data/in.csv > /dev/null
 docker run --name stream-out -d --net=host -v $DATA_FOLDER:/data stream-scripts processOutput.js 9000 /data/out.csv $PREFIX_URL $AMOUNT_TRIPLES > /dev/null
 
@@ -22,7 +19,13 @@ docker run --rm --net=host -v $DATA_FOLDER:/data rml-streamer-stream
 STOPTIME=$(($(date +%s%N)/1000000))
 TIMEDIFFERENCE=`expr $STOPTIME - $STARTIME`
 
-DELAY=`docker run --rm -v $DATA_FOLDER:/data stream-scripts parseTimes.js /data/in.csv /data/out.csv /data/compare.csv`
+echo "id,time" > $DATA_FOLDER/merge.csv
+cat $DATA_FOLDER/in.csv >> merge.csv
+cat $DATA_FOLDER/out.csv >> merge.csv
+sort merge.csv > temp.csv
+mv temp.csv merge.csv
+
+DELAY=`docker run --rm -v $DATA_FOLDER:/data stream-scripts parseTimes.js /data/merge.csv /data/compare.csv`
 
 echo "$1,$TIMEDIFFERENCE,$DELAY" >> $MEASUREMENTS_FILE
 
