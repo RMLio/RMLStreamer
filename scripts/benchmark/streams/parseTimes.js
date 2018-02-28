bcaa9046a5cea6af8fc65556e8148362db58659b
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2018-02-28 13:46:51 +0100

    make parseTimes.js more efficient, only uses one file

--- scripts/benchmark/streams/parseTimes.js
+++ scripts/benchmark/streams/parseTimes.js
@@ -6,55 +6,47 @@
 //modules
 const parseSync = require('csv-parse/lib/sync');
 const parse = require('csv-parse');
-
 const fs    = require('fs');
 
 //arguments
-const fileStart   = process.argv[2];
-const fileStop    = process.argv[3];
-const outputFile  = process.argv[4];
+const inputFile   = process.argv[2];
+const outputFile  = process.argv[3];
 
 //variables
-const fileStopString  = fs.readFileSync(fileStop, {encoding: 'utf-8'});
-
-const stopRecords  = parseSync(fileStopString, {columns:true});
 const stream       = fs.createWriteStream(outputFile, {flags: 'a'});
-const input = fs.createReadStream(fileStart, {encoding: 'utf-8'});
+const input        = fs.createReadStream(inputFile, {encoding: 'utf-8'});
+const inputParser  = parse({columns:true});
 
-const parser = parse({columns:true});
-
-let totalDelay = 0;
+let totalDelay            = 0;
 let totalCompletedRecords = 0;
-let totalRecords = 0;
+let totalRecords          = 0;
 
-parser.on('readable', function(){
-  let record;
 
-  while(record = parser.read()){
-    totalRecords ++;
-    let j = 0;
-
-    while (j < stopRecords.length && stopRecords[j].id !== record.id) {
-      j ++;
-    }
-
-    if (j < stopRecords.length) {
-      const difference = parseInt(stopRecords[j].time) - parseInt(record.time);
-      stream.write(`${record.id},${difference}\n`);
-      totalCompletedRecords ++;
-      totalDelay += difference;
-      stopRecords.splice(j, 1);
-    } else {
-      stream.write(`${record.id},\n`);
-    }
-  }
-});
-
-parser.on('end', () => {
+inputParser.on('end', () => {
   const averageDelay = Math.round(totalDelay/totalCompletedRecords);
   const droppedRecords = totalRecords - totalCompletedRecords;
 
   console.log(`${averageDelay},${droppedRecords}`);
 });
 
-input.pipe(parser);
+let previousRecord;
+
+inputParser.on('data', (record) => {
+  if (previousRecord) {
+    if (previousRecord.id === record.id) {
+      const difference = parseInt(record.time) - parseInt(previousRecord.time);
+      stream.write(`${record.id},${difference}\n`);
+      totalCompletedRecords++;
+      totalDelay += difference;
+      previousRecord = null;
+    } else {
+      stream.write(`${previousRecord.id},\n`);
+      previousRecord = record;
+    }
+  } else {
+    previousRecord = record;
+    totalRecords ++;
+  }
+});
+
+input.pipe(inputParser);
