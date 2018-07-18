43d6546695cbf40200705479082967beb8ca25cf
Sitt Min Oo <sittminoo1995145@gmail.com>
2018-07-18 15:06:34 +0200

    finished figuring out flink clusters

--- src/test/scala/io/rml/framework/StreamingTest.scala
+++ src/test/scala/io/rml/framework/StreamingTest.scala
@@ -6,6 +6,9 @@ import java.util.concurrent.Executors
 import io.rml.framework.helper.fileprocessing.{DataSourceTestHelper, MappingTestHelper}
 import io.rml.framework.helper.{Logger, TestSink}
 import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.configuration.{ConfigConstants, Configuration, TaskManagerOptions}
+import org.apache.flink.runtime.messages.JobManagerMessages.CancelJob
+import org.apache.flink.runtime.minicluster.LocalFlinkMiniCluster
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
 import org.scalatest.AsyncFlatSpec
 
@@ -13,36 +16,82 @@ import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
 
 class StreamingTest extends AsyncFlatSpec {
+
   implicit val env = ExecutionEnvironment.getExecutionEnvironment
-  implicit val senv = StreamExecutionEnvironment.getExecutionEnvironment
+  implicit var senv = StreamExecutionEnvironment.getExecutionEnvironment
   implicit val executor = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
   val passing = "stream"
-
+  var cluster: LocalFlinkMiniCluster = _
   var previous: DataStream[String] = _
 
 
   "TCPsource -pull " should "map the incoming statements correctly with a valid mapping file" in {
-    val generationFuture = prepareGenerationFuture("/home/sitt/Documents/idlab/rml-streamer/src/test/resources/stream/RMLTC0007a-JSON")
+
+    val folder = new File("/home/sitt/Documents/idlab/rml-streamer/src/test/resources/stream/RMLTC0007a-JSON")
+    val folder2 = new File("/home/sitt/Documents/idlab/rml-streamer/src/test/resources/stream/RMLTC0007c-JSON")
+    prepareGenerationFuture(folder.toString)
+    startCluster
+    Logger.logInfo("cluster started")
+
+
+    Thread.sleep(3000)
+    createDataStream(folder)
+    val graph = senv.getStreamGraph
+    graph.setJobName(folder.getName)
+    val jobGraph = graph.getJobGraph
+    cluster.submitJobDetached(jobGraph)
+    val previous = jobGraph.getJobID
+    Logger.logInfo("jobs: " + cluster.currentlyRunningJobs)
+    Logger.logInfo("first started")
+
+
+    Logger.logInfo("Jobmanagers: " + cluster.numJobManagers)
+
     Thread.sleep(5000)
 
-    val folder = new File("/home/sitt/Documents/idlab/rml-streamer/src/test/resources/stream/RMLTC0007c-JSON")
-    val fRML = MappingTestHelper.processFilesInTestFolder(folder.toString)
+    val mnger = cluster.getLeaderGatewayFuture
 
-    Main.createStreamFromFormattedMapping(fRML.head).addSink(TestSink())
-
-    TestUtil.getChCtxFuture.foreach(ctx => {
-      Thread.sleep(2000)
-      val msg = DataSourceTestHelper.processFile(new File("/home/sitt/Documents/idlab/rml-streamer/src/test/resources/stream/example-data.json"))
-      val byteBufMsg = ctx.alloc.buffer(msg.head.length)
-      byteBufMsg.writeBytes(msg.head.getBytes)
-      ctx.channel.writeAndFlush(byteBufMsg)
-    })
+    mnger foreach { gateway =>
+      gateway.ask(new CancelJob(previous), FiniteDuration(1, "seconds"))
+      createDataStream(folder2)
+      val graph2 = senv.getStreamGraph
+      graph2.setJobName(folder2.getName)
+      cluster.submitJobDetached(graph2.getJobGraph)
+      Logger.logInfo("second started")
 
 
+      val sauce = DataSourceTestHelper.processFilesInTestFolder(folder2.toString).flatten
+      Thread.sleep(5000)
+      TestUtil.getChCtxFuture.foreach(ctx => {
 
-    Await.result(generationFuture, Duration.Inf)
+        for (el <- sauce) {
+          val byteBuff = ctx.alloc.buffer(el.length)
+          byteBuff.writeBytes(el.getBytes())
+          ctx.channel.writeAndFlush(byteBuff)
+        }
+      })
+
+
+      Logger.logInfo(cluster.currentlyRunningJobs.toString())
+    }
+    Thread.sleep(Long.MaxValue)
+
+    Await.result(Future(), Duration.Inf)
 
     succeed
+  }
+
+
+
+  def createDataStream(testCaseFolder: File): DataStream[String] = {
+    senv = StreamExecutionEnvironment.createLocalEnvironment()
+    // read the mapping
+    val formattedMapping = MappingTestHelper.processFilesInTestFolder(testCaseFolder.getAbsolutePath)
+    val stream = Main.createStreamFromFormattedMapping(formattedMapping.head)
+
+
+    stream.addSink(TestSink())
+    stream
   }
 
 
@@ -54,34 +103,15 @@ class StreamingTest extends AsyncFlatSpec {
     * @return combined Future[Unit]
     */
   def prepareGenerationFuture(testCaseFolderPath: String): Future[Unit] = {
-    // read the mapping
-    val formattedMapping = MappingTestHelper.processFilesInTestFolder(testCaseFolderPath)
     // read data source file
     val dataSource = DataSourceTestHelper.processFilesInTestFolder(testCaseFolderPath).flatten
 
-    Logger.logInfo(dataSource.toString())
-    // execute
-    previous = Main.createStreamFromFormattedMapping(formattedMapping.head)
-    previous.addSink(TestSink()) //TODO write to collection for assertions
-
-    val listenFuture = startTCPFuture(dataSource)
-    val flinkFuture = startFlinkJob()
-
-    listenFuture flatMap { _ => flinkFuture }
+    startTCPFuture(dataSource)
   }
 
   def startTCPFuture(messages: List[String], port: Int = 9999): Future[Unit] = Future {
     Logger.logInfo(s"Begin TCP server on port: $port ")
     TestUtil.createTCPServer(port, messages.iterator)
-  }
-
-  def startFlinkJob(): Future[Unit] = Future {
-    Logger.logInfo("Started Flink job for rml generation")
-
-    Logger.logInfo(senv.getExecutionPlan)
-    senv.execute("Streaming Test")
-
-    Logger.logInfo("finished")
   }
 
 
---
+++ src/test/scala/io/rml/framework/helper/StreamTestHelper.scala
@@ -0,0 +1,5 @@
+package io.rml.framework.helper
+
+object StreamTestHelper {
+
+}
