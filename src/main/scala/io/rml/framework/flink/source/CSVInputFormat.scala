50161213fa784c2190f0934915699cfa9226b797
Sitt Min Oo <sittminoo1995145@gmail.com>
2018-07-31 13:39:29 +0200

    init

--- src/main/scala/io/rml/framework/flink/source/CSVInputFormat.scala
+++ src/main/scala/io/rml/framework/flink/source/CSVInputFormat.scala
@@ -1,37 +1,17 @@
 package io.rml.framework.flink.source
 
-import java.io.File
-import java.nio.charset.Charset
-
 import io.rml.framework.flink.item.Item
-import io.rml.framework.flink.item.csv.CSVItem
-import org.apache.commons.csv.{CSVFormat, CSVParser, CSVRecord}
-import org.apache.flink.api.common.io.{GenericInputFormat, NonParallelInput}
-import org.apache.flink.core.io.GenericInputSplit
+import org.apache.flink.api.common.io.DelimitedInputFormat
+import org.apache.flink.core.fs.Path
 
-class CSVInputFormat(filePath: String, delimiter: Char = ',', quoteChar: Char = '"') extends GenericInputFormat[Item] with NonParallelInput {
-
-  private var csvIterator: java.util.Iterator[CSVRecord] = _
-
-  override def open(split: GenericInputSplit): Unit = {
-    super.open(split)
-    val csvFile: File = new File(filePath)
-
-    val format: CSVFormat = CSVFormat.newFormat(delimiter)
-      .withQuote(quoteChar)
-      .withTrim()
-      .withFirstRecordAsHeader()
+class CSVInputFormat(filePath: String, delimiter: Char = ',', quoteChar: Char = '"', header: Array[String] = Array.empty) extends DelimitedInputFormat[Item](new Path(filePath), null) {
 
 
-    csvIterator = CSVParser.parse(csvFile, Charset.forName("UTF-8"), format).iterator()
+  override def readRecord(reuse: Item, bytes: Array[Byte], offset: Int, numBytes: Int): Item = {
 
-  }
+    
+    for( index <-  0 until numBytes) {
 
-  override def reachedEnd(): Boolean = !csvIterator.hasNext
-
-  override def nextRecord(reuse: Item): Item = {
-    val record = csvIterator.next()
-    if(record != null) CSVItem(record) else new EmptyItem
-
+    }
   }
 }
---
+++ src/main/scala/io/rml/framework/flink/source/CSVInputSplit.scala
@@ -0,0 +1,5 @@
+package io.rml.framework.flink.source
+
+class CSVInputSplit {
+
+}
