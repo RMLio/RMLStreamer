2144f1431e30cc8e517e09952bf5d94562835d92
Sitt Min Oo <sittminoo1995145@gmail.com>
2018-09-11 16:08:37 +0200

    finished setting up execution order

--- kafka-server-setup.sh
+++ kafka-server-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-
-SCRIPT_USAGE_ERR_MSG="USAGE ./kafka-server-setup.sh -zp [zookeeper.properties] -bp [broker.properties] -cp  [connector_source.properties] [connector_sink.properties]"
+SLEEP_TIME=15
+SCRIPT_USAGE_ERR_MSG="USAGE ./kafka-server-setup.sh -zp [zookeeper.properties] -bp [broker.properties] -cp [connector_standalone.properties] [connector_source.properties] [connector_sink.properties]"
 
 function getProperty {
    PROP_KEY=$2
@@ -48,9 +48,12 @@ do
         ;;
 
         -cp|--connector-properties)
-        #Connector script needs two property files for source and sink
-            CONNECTOR_SOURCE_PROPERTIES="$2"
-            CONNECTOR_SINK_PROPERTIES="$3"
+        #Connector script needs 3 property files for itself, source, and sink
+            CONNECTOR_PROPERTIES="$2"
+            CONNECTOR_SOURCE_PROPERTIES="$3"
+            CONNECTOR_SINK_PROPERTIES="$4"
+
+        shift
         shift
         shift
         shift
@@ -65,15 +68,16 @@ done
 set -- "${POSITIONAL[@]}"
 
 
+KAFKADIR="${KAFKADIR}bin/"
 
+ZOOKEEPER="${KAFKADIR}zookeeper-server-start.sh"
+BROKERSERVER="${KAFKADIR}kafka-server-start.sh"
+CONNECTOR="${KAFKADIR}connect-standalone.sh"
 
-ZOOKEEPER="${KAFKADIR}bin/zookeeper-server-start.sh"
-BROKERSERVER="${KAFKADIR}bin/kafka-server-start.sh"
-CONNECTOR="${KAFKADIR}bin/connect-standalone.sh"
 
 SCRIPT_FILES=("$ZOOKEEPER" "$BROKERSERVER" "$CONNECTOR")
-PROPERTY_FILES=("$ZOOKEEPER_PROPERTY" "$BROKER_PROPERTY" "$CONNECTOR_SOURCE_PROPERTIES" "$CONNECTOR_SINK_PROPERTIES")
-PROPERTY_REF_FILES=("zookeeper" "broker" "connector source"  "connector sink")
+PROPERTY_FILES=("$ZOOKEEPER_PROPERTY" "$BROKER_PROPERTY" "$CONNECTOR_PROPERTIES" "$CONNECTOR_SOURCE_PROPERTIES" "$CONNECTOR_SINK_PROPERTIES")
+PROPERTY_REF_FILES=("zookeeper" "broker" "connector" "connector source"  "connector sink")
 
 
 #Check script files existence
@@ -86,5 +90,32 @@ for i in "${!PROPERTY_FILES[@]}"; do
     checkFileThrowMsg "${PROPERTY_FILES[$i]}" "The required property file for ${PROPERTY_REF_FILES[$i]} doesn't exists:"
 done
 
-
+echo
 echo "----------------------------------------------"
+echo "Starting zookeeper server...."
+
+bash ${ZOOKEEPER} "${ZOOKEEPER_PROPERTY}" &
+ZOOKEEPER_PID=$!
+
+sleep $SLEEP_TIME
+echo
+echo "Starting broker server...."
+echo "-----------------------------------------------"
+bash ${BROKERSERVER} "${BROKER_PROPERTY}" &
+BROKER_PID=$!
+
+sleep $SLEEP_TIME
+echo
+echo "Starting kafka connectors..."
+echo "-----------------------------------------------"
+bash ${CONNECTOR} "${CONNECTOR_PROPERTIES}" "$CONNECTOR_SOURCE_PROPERTIES" "$CONNECTOR_SINK_PROPERTIES" &
+CONNECTOR_PID=$!
+
+sleep $SLEEP_TIME
+kill -9 $CONNECTOR_PID
+
+sleep $SLEEP_TIME
+kill -9 $BROKER_PID
+
+sleep $SLEEP_TIME
+kill -9 $ZOOKEEPER_PID
