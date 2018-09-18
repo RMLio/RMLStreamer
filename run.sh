7c6b046de6ef82b77b1101233e2b7f1ba533cf20
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2018-09-18 15:08:43 +0200

    correc use of config file

--- run.sh
+++ run.sh
@@ -3,72 +3,93 @@
 # RML Framework run script
 #
 # Created by Wouter Maroy
-#
-#
 
-
-
-# Default options are as follows:
-# MAPPINGPATH = src/main/resources/json_stream_data_mapping.ttl
-# OUTPUTPATH =
-# SOCKET = 9000
-
-PROPERTY_FILE=configuration.properties
-
-function getProperty {
+function getPropertyFromConfig {
    PROP_KEY=$1
    PROP_VALUE=`cat $PROPERTY_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
    echo $PROP_VALUE
 }
 
-echo "# Reading property from $PROPERTY_FILE"
-FLINKBIN=$(getProperty "flinkBin")
-MAPPINGPATH=$(getProperty "mappingPath")
-OUTPUTPATH=$(getProperty  "outputPath")
-SOCKET=$(getProperty "socket")
+function setProperties {
+    echo $@
+    # Fetch arguments
+    POSITIONAL=()
+    while [[ $# -gt 0 ]]
+    do
+    key="$1"
 
+    case $key in
+        -p|--path)
+            MAPPINGPATH="$2"
+            shift
+            shift
+            ;;
+        -o|--outputPath)
+            OUTPUTPATH="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -s|--socket)
+            SOCKET="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -f|--flinkBin)
+            FLINKBIN="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -c|--config)
+            CONFIG="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        *)
+            POSITIONAL+=("$1")
+            shift
+            ;;
+    esac
+    done
+    set -- "${POSITIONAL[@]}"
+
+    #echo "mapping: $MAPPINGPATH"
+    #echo "output: $OUTPUTPATH"
+    #echo "socket: $SOCKET"
+    #echo "config: $CONFIG"
+
+    if [ ! -z "$CONFIG" ]; then
+        PROPERTY_FILE=$CONFIG;
+        echo "# Reading property from $PROPERTY_FILE"
+
+        if [ -z "$MAPPINGPATH" ]; then
+          MAPPINGPATH=$(getPropertyFromConfig "mappingPath")
+        fi
+
+        if [ -z "$OUTPUTPATH"  ]; then
+          OUTPUTPATH=$(getPropertyFromConfig "outputPath")
+        fi
+
+        if [ -z "$SOCKET"  ]; then
+          SOCKET=$(getPropertyFromConfig "socket")
+        fi
+
+        if [ -z "$FLINKBIN"  ]; then
+          FLINKBIN=$(getPropertyFromConfig "flinkBin")
+        fi
+    fi
+}
+
+# we pass the script arguments to the function via $@
+setProperties $@
 
 echo "mapping: $MAPPINGPATH"
 echo "output: $OUTPUTPATH"
-echo "socekt: $SOCKET"
+echo "socket: $SOCKET"
 
 echo ""
 echo "// RML Run Script"
 echo "------------------------------------------"
 echo ""
-
-# Fetch arguments
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
-
-case $key in
-	-p|--path)
-	MAPPINGPATH="$2"
-	shift
-	shift
-	;;
-	-o|--outputPath)
-    OUTPUTPATH="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -s|--socket)
-    SOCKET="$2"
-    shift # past argument
-    shift # past value
-    ;;
-	*)
-	POSITIONAL+=("$1")
-	shift
-	;;
-
-
-esac
-done
-set -- "${POSITIONAL[@]}"
-
 
 # Check if $MAPPINGPATH is set
 if [ ! -z "$MAPPINGPATH"  ]; then
