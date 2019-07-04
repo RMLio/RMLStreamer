1a334fcb949e4c635c041bfed7672fbd46224654
Sitt Min Oo <sittminoo1995145@gmail.com>
2019-07-04 10:32:14 +0200

    added documentation to code

--- src/main/scala/io/rml/framework/flink/connector/kafka/PartitionerFormat.scala
+++ src/main/scala/io/rml/framework/flink/connector/kafka/PartitionerFormat.scala
@@ -1,5 +1,14 @@
 package io.rml.framework.flink.connector.kafka
 
+
+/**
+  * Scala enum implementation for determining partitioning format.
+  *
+  * FixedPartitioner will set the output partition to a specific id given by the user as argument.
+  *
+  * DefaultPartitioner will use the default partitioning scheme by Flink.
+  *
+  */
 sealed trait PartitionerFormat
 
 case object FixedPartitioner extends PartitionerFormat
--- src/main/scala/io/rml/framework/flink/connector/kafka/RMLFixedPartitioner.scala
+++ src/main/scala/io/rml/framework/flink/connector/kafka/RMLFixedPartitioner.scala
@@ -2,7 +2,22 @@ package io.rml.framework.flink.connector.kafka
 
 import org.apache.flink.streaming.connectors.kafka.partitioner.FlinkKafkaPartitioner
 
+/**
+  * The partitioner will send all the records to a partition with id specified by the user.
+  * @param partitionID partition id specified by the user
+  */
 class RMLFixedPartitioner(partitionID : Int) extends FlinkKafkaPartitioner[String]{
+
+  /**
+    * Send the record to a particular partition.
+    *
+    * @param record  string record to be partitioned
+    * @param key     key of the record in bytes array
+    * @param value   value of the record in bytes array
+    * @param targetTopic output kafka topic
+    * @param partitions  array of partitions
+    * @return index of the partition to which the record will be sent
+    */
   override def partition(record: String, key: Array[Byte], value: Array[Byte], targetTopic: String, partitions: Array[Int]): Int = {
     if (partitions == null && partitions.length <= 0) {
       throw new IllegalArgumentException("Partitions of the target topic is empty.")
