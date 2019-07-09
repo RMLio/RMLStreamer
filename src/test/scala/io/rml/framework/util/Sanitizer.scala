9c43dcd873d4615cadda51d2c17e32bf3d16764c
Sitt Min Oo <sittminoo1995145@gmail.com>
2019-07-09 16:25:32 +0200

    update sanitize strings for checking

--- src/test/scala/io/rml/framework/util/Sanitizer.scala
+++ src/test/scala/io/rml/framework/util/Sanitizer.scala
@@ -3,6 +3,7 @@ package io.rml.framework.util
 object Sanitizer {
   def sanitize[A <: Iterable[String]](colStrings: A): A = colStrings.map(_.trim
                                                                           .replaceAll(" +", " ")
+                                                                          .replaceAll("> \\.", ">.")
                                                                         )
                                                                     .filter(_.nonEmpty)
                                                                     .asInstanceOf[A]
