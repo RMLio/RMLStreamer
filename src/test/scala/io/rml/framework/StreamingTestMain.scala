bdeb55e1697d29a63b22db8f2de79f92eb8ba389
Sitt Min Oo <sittminoo1995145@gmail.com>
2019-07-25 18:11:48 +0200

    finish test for streaming

--- src/test/scala/io/rml/framework/StreamingTestMain.scala
+++ src/test/scala/io/rml/framework/StreamingTestMain.scala
@@ -45,7 +45,7 @@ object StreamingTestMain {
     val parameters = ParameterTool.fromArgs(args)
 
     val fileName = if (parameters.has(PATH_PARAM)) parameters.get(PATH_PARAM)
-    else "stream/temp/RMLTC0011b-XML"
+    else "stream/temp/RMLTC0020b-XML"
     val testType = if (parameters.has(TYPE_PARAM)) parameters.get(TYPE_PARAM)
     else "kafka"
 
