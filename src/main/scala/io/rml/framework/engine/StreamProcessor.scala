8c8255b5058312712dd01a53a4d6290d7d0e7234
Sitt Min Oo <sittminoo1995145@gmail.com>
2019-07-30 15:56:54 +0200

    finish fixing template generation

--- src/main/scala/io/rml/framework/engine/StreamProcessor.scala
+++ src/main/scala/io/rml/framework/engine/StreamProcessor.scala
@@ -3,19 +3,13 @@ package io.rml.framework.engine
 import io.rml.framework.engine.statement.StatementEngine
 import io.rml.framework.flink.item.{Item, JoinedItem}
 
-abstract class StreamProcessor[T<:Item](engine: StatementEngine[T]) (implicit postProcessor: PostProcessor) extends  Processor[T, Iterable[T]](engine) {
+abstract class StreamProcessor[T <: Item](engine: StatementEngine[T])(implicit postProcessor: PostProcessor) extends Processor[T, Iterable[T]](engine) {
   override def map(in: Iterable[T]): List[String] = {
     if (in.isEmpty) return List()
 
-    try {
-      val triples = in flatMap engine.process
-      postProcessor.process(triples)
+    val triples = in flatMap engine.process
+    postProcessor.process(triples)
 
-
-    }catch{
-      case e => println(e)
-                List()
-    }
   }
 }
 
