3d6871f944d4ed717b61673d2b29ee4646ee5464
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2020-03-03 13:24:56 +0100

    License

--- src/main/scala/io/rml/framework/core/util/Util.scala
+++ src/main/scala/io/rml/framework/core/util/Util.scala
@@ -1,3 +1,27 @@
+/**
+  * MIT License
+  *
+  * Copyright (C) 2017 - 2020 RDF Mapping Language (RML)
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in
+  * all copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  * THE SOFTWARE.
+  *
+  **/
 package io.rml.framework.core.util
 
 import java.io.{ByteArrayInputStream, File, FileInputStream, InputStream}
@@ -7,7 +31,7 @@ import java.nio.file.{Files, Paths}
 import java.util.regex.Pattern
 
 import io.rml.framework.core.extractors.MappingReader
-import io.rml.framework.core.model.{FormattedRMLMapping, Literal}
+import io.rml.framework.core.model.FormattedRMLMapping
 import io.rml.framework.shared.ReadException
 
 import scala.collection.mutable.ListBuffer
