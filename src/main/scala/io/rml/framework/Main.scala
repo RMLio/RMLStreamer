8c8da97325e39a789f5f8099b0a84e295f525346
Sitt Min Oo <sittminoo1995145@gmail.com>
2021-02-14 22:54:21 +0100

    added mini cross join for batches of iteritems matching the key for the bigger join

--- src/main/scala/io/rml/framework/Main.scala
+++ src/main/scala/io/rml/framework/Main.scala
@@ -274,9 +274,7 @@ object Main extends Logging {
 
 
         joined.apply((firstIterItems, secondIterItems) => {
-          // TODO: Check what iterItems actually contain and see if there is a need to do a mini cross join
-          val joined = JoinedItem(firstIterItems.head, secondIterItems.head)
-          List(joined)
+          firstIterItems.flatMap( item1 => secondIterItems.map( item2 => JoinedItem(item1, item2)))
         })
           .flatMap(joined => joined)
 
@@ -290,16 +288,16 @@ object Main extends Logging {
       } else { // if there are no join conditions a cross join will be executed
         // add pseudo key for all incoming elements
         val key = tm.logicalSource.semanticIdentifier
-        // create a crossed data stream
+        // create a cross join data stream where all items are joined
+        // on a default fixed key
         val crossed = childDataStream.join(parentDataStream)
           .where( _ => key)
           .equalTo( _ => key)
           .window(TumblingEventTimeWindows.of(Time.milliseconds(2)))
 
         crossed.apply((firstIterItems, secondIterItems) => {
-          // TODO: Check what iterItems actually contain and see if there is a need to do a mini cross join
-          val joined = JoinedItem(firstIterItems.head, secondIterItems.head)
-          List(joined)
+          // mini cross join for all iteritems
+         firstIterItems.flatMap( item1 => secondIterItems.map( item2 => JoinedItem(item1, item2)))
         })
           .flatMap(joined => joined)
 
