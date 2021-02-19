5200e11f05b2a2311a91a68d836d86b03f3fdc05
Sitt Min Oo <sittminoo1995145@gmail.com>
2021-02-20 00:49:26 +0100

    refactored testing and changed cache's value type

--- src/main/scala/io/rml/framework/core/extractors/JoinConfigMapCache.scala
+++ src/main/scala/io/rml/framework/core/extractors/JoinConfigMapCache.scala
@@ -1,6 +1,6 @@
 package io.rml.framework.core.extractors
 
-import io.rml.framework.core.model.TermMap
+import io.rml.framework.core.model.JoinConfigMap
 
-object JoinConfigMapCache extends scala.collection.mutable.HashMap[String, TermMap]
+object JoinConfigMapCache extends scala.collection.mutable.HashMap[String, JoinConfigMap]
 
