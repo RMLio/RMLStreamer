ba67d7611d9d524541a5128d2434c173bd7e721a
Michiel Derveeuw <michiel.derveeuw@ugent.be>
2021-07-01 12:12:16 +0200

    Replace tabs by spaces

--- src/main/scala/io/rml/framework/flink/sink/SPARQLSink.scala
+++ src/main/scala/io/rml/framework/flink/sink/SPARQLSink.scala
@@ -17,51 +17,51 @@ import java.nio.charset.StandardCharsets
 
 
 class SPARQLSink private[sink](val URL: String) extends RichSinkFunction[String] {
-	private var httpClient: CloseableHttpAsyncClient = _
+  private var httpClient: CloseableHttpAsyncClient = _
 
-	@throws[Exception]
-	override def open(parameters: Configuration): Unit = {
-		httpClient = HttpAsyncClients.custom
-			.setKeepAliveStrategy(DefaultConnectionKeepAliveStrategy.INSTANCE)
-			.disableCookieManagement
-			.build
-		httpClient.start()
-	}
+  @throws[Exception]
+  override def open(parameters: Configuration): Unit = {
+    httpClient = HttpAsyncClients.custom
+      .setKeepAliveStrategy(DefaultConnectionKeepAliveStrategy.INSTANCE)
+      .disableCookieManagement
+      .build
+    httpClient.start()
+  }
 
-	@throws[Exception]
-	override def invoke(value: String, context: SinkFunction.Context): Unit = {
-		val httpPost = new HttpPost(URL)
-		httpPost.setEntity(new StringEntity(s"INSERT DATA {$value}"))
-		httpPost.setHeader("Content-Type", "application/sparql-update")
-		httpClient.execute(httpPost, new FutureCallback[HttpResponse] {
-			override def completed(result: HttpResponse): Unit = {
-				val httpStatusCode = result.getStatusLine.getStatusCode
-				if (httpStatusCode >= 300) {
-					val body = result.getEntity
-					if (body.getContentLength > 0) try {
-						val inputStream = body.getContent
-						try {
-							val contents = new String(IOUtils.toByteArray(inputStream), StandardCharsets.UTF_8)
-							logError("Error while sending SPARQL request: " + contents)
-						} catch {
-							case e: IOException => logError("Error while sending SPARQL request")
-						} finally if (inputStream != null) inputStream.close()
-					}
-				}
-			}
+  @throws[Exception]
+  override def invoke(value: String, context: SinkFunction.Context): Unit = {
+    val httpPost = new HttpPost(URL)
+    httpPost.setEntity(new StringEntity(s"INSERT DATA {$value}"))
+    httpPost.setHeader("Content-Type", "application/sparql-update")
+    httpClient.execute(httpPost, new FutureCallback[HttpResponse] {
+      override def completed(result: HttpResponse): Unit = {
+        val httpStatusCode = result.getStatusLine.getStatusCode
+        if (httpStatusCode >= 300) {
+          val body = result.getEntity
+          if (body.getContentLength > 0) try {
+            val inputStream = body.getContent
+            try {
+              val contents = new String(IOUtils.toByteArray(inputStream), StandardCharsets.UTF_8)
+              logError("Error while sending SPARQL request: " + contents)
+            } catch {
+              case e: IOException => logError("Error while sending SPARQL request")
+            } finally if (inputStream != null) inputStream.close()
+          }
+        }
+      }
 
-			override def failed(ex: Exception): Unit = {
-				logError("Error while sending SPARQL request")
-			}
+      override def failed(ex: Exception): Unit = {
+        logError("Error while sending SPARQL request")
+      }
 
-			override def cancelled(): Unit = {
-				logError("Error while sending SPARQL request")
-			}
-		})
-	}
+      override def cancelled(): Unit = {
+        logError("Error while sending SPARQL request")
+      }
+    })
+  }
 
-	@throws[Exception]
-	override def close(): Unit = {
-		httpClient.close()
-	}
+  @throws[Exception]
+  override def close(): Unit = {
+    httpClient.close()
+  }
 }
