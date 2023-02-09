0ac3f5dc675544ccfb9bdf419395560db2d185b2
Jozef Jankaj <jozef.jankaj@ugent.be>
2023-02-09 13:32:53 +0100

    initial passing version of source function

--- src/main/scala/io/rml/framework/flink/source/functions/RDBSourceFunction.scala
+++ src/main/scala/io/rml/framework/flink/source/functions/RDBSourceFunction.scala
@@ -1,24 +1,27 @@
 package io.rml.framework.flink.source.functions
 
 import be.ugent.idlab.knows.dataio.access.Access
+import be.ugent.idlab.knows.dataio.iterators.CSVSourceIterator
 import be.ugent.idlab.knows.dataio.source.CSVSource
 import io.rml.framework.core.internal.Logging
-import io.rml.framework.flink.source.throwaway.SerializableIterator
-import org.apache.flink.streaming.api.functions.source.SourceFunction
+import org.apache.flink.streaming.api.functions.source.{RichSourceFunction, SourceFunction}
 
 /**
  * Implementation of getting data from RDB
  */
-class RDBSourceFunction(access: Access) extends SourceFunction[CSVSource] with Logging with Serializable {
+class RDBSourceFunction(access: Access) extends RichSourceFunction[CSVSource] with Logging with Serializable {
 
-  val iterator = new SerializableIterator(access)
+//  @transient
+//  val iterator: CSVSourceIterator = new CSVSourceIterator()
+//  iterator.open(access)
   var running = true
 
   override def run(sourceContext: SourceFunction.SourceContext[CSVSource]): Unit = {
-
+    val iterator = new CSVSourceIterator()
+    iterator.open(access)
     while (running && iterator.hasNext) {
       sourceContext.getCheckpointLock.synchronized {
-        val item = this.iterator.next()
+        val item = iterator.next()
         logInfo(item.toString)
         sourceContext.collect(item.asInstanceOf[CSVSource])
       }
--- src/test/resources/rml-testcases/RMLTC0000-PostgreSQL/resource.sql
+++ src/test/resources/rml-testcases/RMLTC0000-PostgreSQL/resource.sql
@@ -1,6 +1,4 @@
 DROP TABLE IF EXISTS student CASCADE ;
 CREATE TABLE student (
-                         "ID" INTEGER,
-                         "Name" VARCHAR(50)
+    "Name" VARCHAR(50)
 );
-INSERT INTO student values ('10', 'Venus');
